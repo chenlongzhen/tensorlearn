e810b2dbafa9344d0f6d03042612e7af8cc58516
chenlongzhen <longzhen.chen@foxmail.com>
2017-03-08 17:33:40 +0800

    init

---
+++ script/cnn.py
@@ -0,0 +1,83 @@
+#encoding=utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
+mnist = input_data.read_data_sets("../MNIST_data", one_hot=True)
+
+def weight_variable(shape):
+	initial = tf.truncated_normal(shape, stddev=0.1)
+	return tf.Variable(initial)
+
+def bias_variable(shape):
+	initial = tf.constant(0.1, shape=shape)
+	return tf.Variable(initial)
+
+def conv2d(x,W):
+	return tf.nn.conv2d(x,W,strides=[1,1,1,1], padding='SAME')
+
+def max_pool_2x2(x):
+	return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+
+#data
+x = tf.placeholder(tf.float32,[None,784])
+y_ = tf.placeholder(tf.float32,[None,10])
+x_image = tf.reshape(x,[-1,28,28,1]) #第二三是图片的宽高 四是通道数量
+
+#第一层
+#权重初始化
+W_conv1 = weight_variable([5,5,1,32])
+b_conv1 = bias_variable([32])
+
+h_conv1= tf.nn.relu(conv2d(x_image, W_conv1)+b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+#第二层
+W_conv2 = weight_variable([5,5,32,64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+#全联接层
+W_fc1 = weight_variable([7*7*64,1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+#drop out
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# out layer soft max
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+# train and evaluation
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y_conv))
+
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+# Train
+for i in range(20000):
+	batch_xs, batch_ys = mnist.train.next_batch(50)
+	# Test trained model
+	sess.run(train_step, feed_dict={x:batch_xs, y_: batch_ys,keep_prob:0.5}) # drop out is 0.5
+	if i % 100 == 0:
+		print("iter {}".format(i))
+		print(sess.run(accuracy, feed_dict={x: mnist.test.images,y_: mnist.test.labels,keep_prob:1.0})) # test drop out is 1
---
+++ script/softmax_demo.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
+mnist = input_data.read_data_sets("../MNIST_data", one_hot=True)
+
+# Create the model
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.matmul(x, W) + b
+
+# Define loss and optimizer
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+# The raw formulation of cross-entropy,
+#
+#   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
+#                                 reduction_indices=[1]))
+#
+# can be numerically unstable.
+#
+# So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
+# outputs of 'y', and then average across the batch.
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+#evalue
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+# Train
+for i in range(10000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+	# Test trained model
+	if i % 100 == 0:
+		print("iter {}".format(i))
+		print(sess.run(accuracy, feed_dict={x: mnist.test.images,y_: mnist.test.labels}))
+
+
+
