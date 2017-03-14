1b8ab5b04623498dde041de9048f2171e5b7bb14
chenlongzhen <760950023@qq.com>
2017-03-14 10:55:56 +0800

    yapf;tf version check

--- script/finetune_alexnet_with_tensorflow/datagenerator.py
+++ script/finetune_alexnet_with_tensorflow/datagenerator.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-
 """
 This code is highly influenced by the implementation of:
 https://github.com/joelthchao/tensorflow-finetune-flickr-style/dataset.py
@@ -11,12 +10,16 @@ Keras library. But as I needed BGR color format for fine-tuneing AlexNet I
 wrote my own little generator.
 """
 
+
 class ImageDataGenerator:
-    def __init__(self, class_list, horizontal_flip=False, shuffle=False, 
-                 mean = np.array([104., 117., 124.]), scale_size=(227, 227),
-                 nb_classes = 2):
-        
-                
+    def __init__(self,
+                 class_list,
+                 horizontal_flip=False,
+                 shuffle=False,
+                 mean=np.array([104., 117., 124.]),
+                 scale_size=(227, 227),
+                 nb_classes=2):
+
         # Init params
         self.horizontal_flip = horizontal_flip
         self.n_classes = nb_classes
@@ -24,13 +27,13 @@ class ImageDataGenerator:
         self.mean = mean
         self.scale_size = scale_size
         self.pointer = 0
-        
+
         self.read_class_list(class_list)
-        
+
         if self.shuffle:
             self.shuffle_data()
 
-    def read_class_list(self,class_list):
+    def read_class_list(self, class_list):
         """
         Scan the image file and get the image paths and labels
         """
@@ -42,38 +45,37 @@ class ImageDataGenerator:
                 items = l.split()
                 self.images.append(items[0])
                 self.labels.append(int(items[1]))
-            
+
             #store total number of data
             self.data_size = len(self.labels)
-        
+
     def shuffle_data(self):
         """
         Random shuffle the images and labels
         """
-		# python 3....
+        # python 3....
         #images = self.images.copy()
         #labels = self.labels.copy()
         images = self.images
         labels = self.labels
         self.images = []
         self.labels = []
-        
+
         #create list of permutated index and shuffle data accoding to list
         idx = np.random.permutation(len(labels))
         for i in idx:
             self.images.append(images[i])
             self.labels.append(labels[i])
-                
+
     def reset_pointer(self):
         """
         reset pointer to begin of the list
         """
         self.pointer = 0
-        
+
         if self.shuffle:
             self.shuffle_data()
-        
-    
+
     def next_batch(self, batch_size):
         """
         This function gets the next n ( = batch_size) images from the path list
@@ -82,26 +84,27 @@ class ImageDataGenerator:
         # Get next batch of image (path) and labels
         paths = self.images[self.pointer:self.pointer + batch_size]
         labels = self.labels[self.pointer:self.pointer + batch_size]
-        
+
         #update pointer
         self.pointer += batch_size
-        
+
         # Read images
-        images = np.ndarray([batch_size, self.scale_size[0], self.scale_size[1], 3])
+        images = np.ndarray(
+            [batch_size, self.scale_size[0], self.scale_size[1], 3])
         for i in range(len(paths)):
             img = cv2.imread(paths[i])
-            
+
             #flip image at random if flag is selected
             if self.horizontal_flip and np.random.random() < 0.5:
                 img = cv2.flip(img, 1)
-            
+
             #rescale image
             img = cv2.resize(img, (self.scale_size[0], self.scale_size[0]))
             img = img.astype(np.float32)
-            
+
             #subtract mean
             img -= self.mean
-                                                                 
+
             images[i] = img
 
         # Expand labels to one hot encoding
